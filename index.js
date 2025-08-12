a0ee5e1b4aff1b4eea29cfd36a9aaf1689cd71ee
Daniel Abernathy <dabernathy89@gmail.com>
2025-08-12 16:30:28 -0500

    feat: feature 4

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.get('/feature-3', (req, res) => {
   res.send('Hello from another feature!')
 })
 
+app.get('/feature-4', (req, res) => {
+  res.send('Hello from another feature!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
